1dac9a4ba89d18d0f31f287a6e4cde67422109e6
Ali Dakkak <alidakak21@gmail.com>
2024-03-05 20:43:04 +0300

    gmail

--- app/Http/Controllers/InviteeController.php
+++ app/Http/Controllers/InviteeController.php
@@ -97,7 +97,7 @@ class InviteeController extends Controller
                 $number_of_people += $invitee['count'];
                 $this->generateQRCodeForInvitee($newInvitee->id);
             }
-            $userEmail = 'Lele881@hotmail.com';
+            $userEmail = 'lulumhmd762@gmail.com';
             $link = $newInvitee->link;
             EmailService::sendHtmlEmail($userEmail, $link);
             DB::commit();

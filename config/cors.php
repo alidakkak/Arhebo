bca426747b16118e47faf9239fcc30522cf31d79
Ali Dakkak <alidakak21@gmail.com>
2024-05-26 14:36:21 +0300

    cors

--- config/cors.php
+++ config/cors.php
@@ -15,7 +15,10 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => [
+        'api/*', 'sanctum/csrf-cookie',
+        public_path('/templates_image/*')
+    ],
 
     'allowed_methods' => ['*'],
 

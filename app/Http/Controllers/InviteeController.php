55de367c39b91af9668510fd711e99272a8edda0
Ali Dakkak <alidakak21@gmail.com>
2024-09-03 12:10:51 +0300

    pushNitf

--- app/Http/Controllers/InviteeController.php
+++ app/Http/Controllers/InviteeController.php
@@ -155,7 +155,6 @@ class InviteeController extends Controller
             $invitation->save();
             $image = $invitation->Template ? $invitation->Template->image : null;
             $whatsApp_template = $this->whatsApp_template($invitation->id);
-            return $whatsApp_template;
             $this->sendWhatsAppMessages($inviteesForWhatsapp->toArray(), url($image), $whatsApp_template);
 
             DB::commit();

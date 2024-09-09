2f1f5110eb2ec3b1f6428405a1c0818bc8edd0d4
Ali Dakkak <alidakak21@gmail.com>
2024-09-09 16:21:27 +0300

    casc

--- app/Http/Controllers/InviteeController.php
+++ app/Http/Controllers/InviteeController.php
@@ -38,7 +38,7 @@ class InviteeController extends Controller
                 'customParams' => [
                     ['name' => 'product_image_url', 'value' => $image],
                     ['name' => 'nice_sentence', 'value' => $whatsApp_template],
-                    ['name' => 'name', 'value' => "{$invitee['name']}%0a"],
+                    ['name' => 'name', 'value' => "{$invitee['name']}"],
                     ['name' => '1', 'value' => $invitee['link']],
                 ],
             ];

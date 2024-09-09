54c35dabc82ffce9bbf6f2e3929216577aa475af
Ali Dakkak <alidakak21@gmail.com>
2024-09-09 15:56:58 +0300

    casc

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -50,7 +50,7 @@ class AuthController extends Controller
                     "device_token" => $request->device_token
                 ]);
             }else{
-                DeviceToken::create(array_merge($request->only(['device_token'] , $user->id)));
+                DeviceToken::create(array_merge($request->only(['device_token'] , ['user_id' => $user->id])));
             }
         }
         return $this->createNewToken($token);

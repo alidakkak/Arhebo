edb01b595afa0f9fa73b5d4087bd6bc53d88cf6a
Ali Dakkak <alidakak21@gmail.com>
2024-09-22 21:53:19 +0300

    a

--- database/seeders/TemplateJawadSeeder.php
+++ database/seeders/TemplateJawadSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Category;
-use IlluminateSupportFacadesDB; 
+use Illuminate\Support\Facades\DB; 
 
 class TemplateJawadSeeder extends Seeder
 {

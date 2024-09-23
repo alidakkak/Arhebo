0358d60edd2f1e67b354b9b72ccdf861a901f068
Ali Dakkak <alidakak21@gmail.com>
2024-09-23 20:37:31 +0300

    final data :)

--- app/Services/WhatsAppDeleteInvitationService.php
+++ app/Services/WhatsAppDeleteInvitationService.php
@@ -16,29 +16,32 @@ class WhatsAppDeleteInvitationService
         $this->apiToken = env('WHATSAPP_API_URL_SEND_TEMPLATE_MESSAGES');
     }
 
-    public function sendWhatsAppMessages(array $invitees, $event_name)
+    public function sendWhatsAppMessages(array $invitees, string $eventName)
     {
-        $receivers = [];
-
-        foreach ($invitees as $invitee) {
-            $receivers[] = [
-                'whatsappNumber' => $invitee['phone'],
+        // Prepare the receivers' data
+        $receivers = array_map(function ($invitee) use ($eventName) {
+            return [
+                'whatsappNumber' => $invitee['phone'], // Ensure phone number exists in the invitee
                 'customParams' => [
-                    ['name' => 'event_name', 'value' => $event_name],
-                ],
+                    [
+                        'name' => 'event_name',
+                        'value' => $eventName
+                    ]
+                ]
             ];
-        }
-        dd($receivers);
+        }, $invitees);
 
+        // Send the HTTP POST request to the WhatsApp API
         $response = Http::withHeaders([
-            'Authorization' => 'Bearer '.$this->apiToken,
-            'Content-Type' => 'application/json',
+            'Authorization' => 'Bearer ' . $this->apiToken, // Authorization header
+            'Content-Type' => 'application/json', // Ensure JSON content-type
         ])->post($this->apiUrl, [
-            'template_name' => 'cancel_invitation',
-            'broadcast_name' => 'cancel_invitation',
-            'receivers' => $receivers,
+            'template_name' => 'cancel_invitation', // Name of the template
+            'broadcast_name' => 'cancel_invitation', // Broadcast name for the message
+            'receivers' => $receivers, // List of receivers with their custom params
         ]);
 
+        // Return the response in JSON format
         return $response->json();
     }
 }

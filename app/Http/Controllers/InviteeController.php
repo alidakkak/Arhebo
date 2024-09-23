7b4626d3d52b2cf7c229ca92b4e9126ce6a00143
Ali Dakkak <alidakak21@gmail.com>
2024-09-23 09:07:43 +0300

    final data :)

--- app/Http/Controllers/InviteeController.php
+++ app/Http/Controllers/InviteeController.php
@@ -265,8 +265,9 @@ class InviteeController extends Controller
             imagepng($webpImage, $tempPngPath);
             imagedestroy($webpImage);
             $whatsApp_template = $this->whatsApp_template($invitation->id);
+            return $whatsApp_template;
             $this->sendWhatsAppMessages($inviteesForWhatsapp->toArray(), asset('/temp/temp_image.png'), $whatsApp_template);
-//            File::delete($tempPngPath);
+            File::delete($tempPngPath);
             DB::commit();
 
             return response()->json([

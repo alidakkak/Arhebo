da1098808f284696d3c8b9802d8eff186bba4abc
Ali Dakkak <alidakak21@gmail.com>
2024-10-01 16:34:32 +0300

    final data :)

--- app/Http/Controllers/InviteeController.php
+++ app/Http/Controllers/InviteeController.php
@@ -651,7 +651,7 @@ class InviteeController extends Controller
         ]);
 
         $whatsAppDesignerFinishingService = new WhatsAppDesignerFinishingService;
-        dd($user->phone);
+        dd($invitation->event_name);
         $whatsAppDesignerFinishingService->WhatsAppDesignerFinishing($user->phone, $invitation->event_name,
             $invitation->inviter);
 

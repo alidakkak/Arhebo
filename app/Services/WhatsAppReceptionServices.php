f762a831624e564f1194a99577dc48b9bd2b6e9c
Ali Dakkak <alidakak21@gmail.com>
2024-10-01 12:19:49 +0300

    final data :)

--- app/Services/WhatsAppReceptionServices.php
+++ app/Services/WhatsAppReceptionServices.php
@@ -38,7 +38,7 @@ class WhatsAppReceptionServices
         if (! $receiver['isValidWhatsAppNumber']) {
             return [
                 'status' => false,
-                'message' => 'الرقم ' . $phone . ' غير متوفر على WhatsApp.',
+                'message' => 'الرقم ' . $phone . ' غير متوفر على وتساب.',
             ];
         }
             return [
